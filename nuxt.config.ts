4d1b7295aeae19baa3ee6c40a6ed813696a82fae
Samuel Petre <petresamuel@gmail.com>
2024-11-11 13:24:06 -0300

    exclude /static from sitemap

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -94,7 +94,8 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 
   sitemap: {
     hostname: 'https://psicologanataliaferreira.com',
-    routes: ['/']
+    routes: ['/'],
+    exclude: ['/static']
   },
 
   robots: {
