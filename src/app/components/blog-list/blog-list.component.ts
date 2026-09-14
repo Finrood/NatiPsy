d323360ff02522c0f64287612cca1d1909c10c07
Finrood <petresamuel@gmail.com>
2026-09-14 15:21:30 -0300

    fix(audit): restore blog date helpers

--- src/app/components/blog-list/blog-list.component.ts
+++ src/app/components/blog-list/blog-list.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit, OnDestroy, ChangeDetectionStrategy, ChangeDetectorRef, inject } from '@angular/core';
 import { BlogService } from '../../services/blog.service';
-import { BlogPost, blogImageUrl } from '../../models/blog-post.model';
+import {
+  BlogPost,
+  blogDateOnly,
+  blogImageUrl,
+  formatBlogDate,
+} from '../../models/blog-post.model';
 import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { RouterLink, ActivatedRoute, Router } from '@angular/router';
 import { SeoService } from '../../services/seo.service';
